2cb9c79d54ea6443c9be4464bd100f292faf207e
Arun prasadh <13350352+arun-prasadh@users.noreply.github.com>
2023-11-24 20:31:09 +0530

    Update __init__.py

--- solarathon/pages/__init__.py
+++ solarathon/pages/__init__.py
@@ -1,36 +1,32 @@
 import solara
 
-# Declare reactive variables at the top level. Components using these variables
-# will be re-executed when their values change.
-sentence = solara.reactive("Solara makes our team more productive.")
-word_limit = solara.reactive(10)
-
-
-# in case you want to override the default order of the tabs
-route_order = ["/", "settings", "chat", "clickbutton"]
+text = solara.reactive("")
+input_values = solara.reactive([])
+display_text = solara.reactive("")
+Food = solara.reactive(0)
+github_url = "https://en.wikipedia.org/wiki/Biryani"
 
 @solara.component
 def Page():
-    with solara.Column(style={"padding-top": "30px"}):
-        solara.Title("Solarathon example project")
-        # Calculate word_count within the component to ensure re-execution when reactive variables change.
-        word_count = len(sentence.value.split())
+  def add_value():
+        input_values.value.append("Question : " + text.value)
+        input_values.value.append("Answer : "+display_text.value)        
+  def remove_value():
+      input_values.value=[]
+      text.value=""
+      display_text.value=""
 
-        solara.SliderInt("Word limit", value=word_limit, min=2, max=20)
-        solara.InputText(label="Your sentence", value=sentence, continuous_update=True)
-
-        # Display messages based on the current word count and word limit.
-        if word_count >= int(word_limit.value):
-            solara.Error(f"With {word_count} words, you passed the word limit of {word_limit.value}.")
-        elif word_count >= int(0.8 * word_limit.value):
-            solara.Warning(f"With {word_count} words, you are close to the word limit of {word_limit.value}.")
-        else:
-            solara.Success("Great short writing!")
-
-        solara.Markdown("*First exercise*: remove this text and write your own sentence.")
-
-
-@solara.component
-def Layout(children):
-    # this is the default layout, but you can override it here, for instance some extra padding
-    return solara.AppLayout(children=children, style={"padding": "20px"})
+  solara.InputText("Ask a Question", value=text)
+  if text.value =="Good Morning":
+      display_text.value="Hi Good Morning!!"
+  elif text.value:
+     display_text.value="We dont have this Info"
+  solara.Markdown(f"**Answer**: {display_text.value}")
+  with solara.Row():
+        solara.Button("Clear", on_click=remove_value)
+        solara.Button("Enter", on_click=add_value)
+  solara.Markdown(f"**All Questions**: {input_values.value}")
+  with solara.Card("Frequently Asked Questions"):
+    solara.Button(label="Briyani", icon_name="mdi-github-circle", attributes={"href": github_url, "target": "_blank"}, text=True, outlined=True)
+    solara.Markdown(f"""`Food`       
+        [Briyani](https://en.wikipedia.org/wiki/Biryani)""")
