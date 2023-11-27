b424096b73672efb8863fcebdd283a8e3cbcae01
Janet Mardjuki <janetmardjuki@gmail.com>
2023-11-26 22:54:39 -0800

    Added card for the faq changes

--- solarathon/pages/__init__.py
+++ solarathon/pages/__init__.py
@@ -1,32 +1,35 @@
 import solara
-import csv
-import os 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+import json
+import os
+from pathlib import Path
+
 
 # Declare reactive variables at the top level. Components using these variables
 # will be re-executed when their values change.
 sentence = solara.reactive("Solara makes our team more productive.")
 word_limit = solara.reactive(10)
 
-
 # in case you want to override the default order of the tabs
 route_order = ["/", "settings", "chat", "clickbutton"]
 
 # load csv
-#with open('test.csv', newline='') as csvfile:
-#    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+def load_json(filepath):
+    HERE = Path(__file__).parent
+    json_file_path = (HERE / filepath)
+    with open(json_file_path, encoding="utf-8-sig") as file:
+        return json.load(file)
+
+filepath = 'input_test.json'
+qa_json= load_json(filepath)
 
 @solara.component
 def Page():
     with solara.Column(style={"padding-top": "30px"}):
-        solara.Title("Solara FAQs")
-        with solara.Card("Frequently Asked Question"):
-            # Change this with the number of the df/csv list
-            for i in range(0, 11): 
-                with solara.Columns([0, 1]):
-                    solara.Markdown("Question")
-                    solara.Markdown("Answer")
+        solara.Title("Solara Frequently Asked Question")
+        # Change this with the number of the df/csv list
+        for row in qa_json: 
+            with solara.Card(title=row["question"]):
+                solara.Markdown(row["answer"])
 
 
 @solara.component
